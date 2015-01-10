db17e73f5b248d4ee1757f5fe89c08a6c9b87f88
Kuber Kaul <kuberkaul@icloud.com>
2015-01-10 17:57:46 -0500

    correcting models.py

--- krishimoney/models.py
+++ krishimoney/models.py
@@ -8,29 +8,29 @@ class cultivation(models.Model):
 	varieties = models.CharField(max_length=50)
 	plantingSeasonStart = models.CharField(max_length=50)
 	plantingSeasonEnd = models.CharField(max_length=50)
-	harvestManagementStart = models.CharField(maxlength=50) 
+	harvestManagementStart = models.CharField(max_length=50) 
 	harvestManagementEnd = 	models.CharField(max_length=50)
 
-class plantingSpecification(models.Model)
+class plantingSpecification(models.Model):
 	name = models.CharField(max_length=50)
 	spacingLength = models.IntegerField(default=80)
 	spacingBreadth = models.IntegerField(default=80)
-	plantPopulationPerAcre = models.IntegerFied(default=80)
+	plantPopulationPerAcre = models.IntegerField(default=80)
 	plotSize = models.IntegerField(default=80)
 	
-class irrigation(models.Model)
+class irrigation(models.Model):
 	name = models.CharField(max_length=50)
 	irrigationSystem = models.CharField(max_length=50)
 	irrigationPerPlant = models.IntegerField(default=80)
 	irrigationPerAcrePerDay = models.IntegerField(default=80)
 	dischargePerDay =  models.IntegerField(default=80)
 
-class plantProtection(models.Model)
+class plantProtection(models.Model):
 	name = models.CharField(max_length=50)
 	pests = models.CharField(max_length=50)
 	diseases = models.CharField(max_length=50)
 
-class priceLastThreeYears(models.Model)
+class priceLastThreeYears(models.Model):
 	firstYear = models.IntegerField(default=80)
 	secondYear = models.IntegerField(default=80)
 	secondYear = models.IntegerField(default=80)
