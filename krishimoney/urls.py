0b321acd8a5f3bb4353bd1f37a390e78d1c8eb63
Nandita Rao <nanditarao@Nanditas-MacBook-Pro.local>
2015-01-10 15:53:28 -0500

    Adding the about us page

--- krishimoney/urls.py
+++ krishimoney/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns("",
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^account/", include("account.urls")),
+    url(r"^aboutUs/", TemplateView.as_view(template_name="aboutUs.html"),name="aboutUs"),
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
