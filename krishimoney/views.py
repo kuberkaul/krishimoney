cf8eea8e0cd0e2f56ead0d8437b32079656f42c6
Nandita Rao <nanditarao@Nanditas-MacBook-Pro.local>
2015-01-17 15:34:59 -0500

    Form submit action

--- krishimoney/views.py
+++ krishimoney/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponse
+
 def adminForm(request, id):
     return HttpResponse("You're looking at question %s." % question_id)
 
-def adminPagesubmitted(request, id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
+def adminPageSubmitted(id):
+    print "Hi" + str(id)
+    return HttpResponse(id)
