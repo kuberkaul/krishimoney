7b0ca72dcae36d06ad5fa6f642df2144e20d5736
Nandita Rao <nr2445@columbia.edu>
2015-01-17 20:25:15 -0500

    Adding django form to match the admin form and modify data

--- krishimoney/urls.py
+++ krishimoney/urls.py
@@ -11,8 +11,8 @@ urlpatterns = patterns("",
     url(r"^admin/", include(admin.site.urls)),
     url(r"^account/", include("account.urls")),
     url(r"^aboutUs/", TemplateView.as_view(template_name="aboutUs.html"),name="aboutUs"),
-    url(r"^adminForm/", TemplateView.as_view(template_name="admin_form.html"),name="adminform"),
-    url(r"^adminPageSubmitted/", views.adminPageSubmitted)
+    url(r"^adminForm/", views.adminForm),
+    url(r"^adminPageSubmitted/", views.adminForm)
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- krishimoney/views.py
+++ krishimoney/views.py
@@ -1,9 +1,32 @@
 from django.http import HttpResponse
 from django.template import RequestContext
+from krishimoney.forms import AdminForm
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 def adminForm(request, id):
     return HttpResponse("You're looking at question %s." % question_id)
 
 def adminPageSubmitted(request):
-    print "Hi" + str(request)
+    print "Hi" + str(request.POST['cropName'])
     return HttpResponse("You're looking at question")
+
+def adminForm(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        print "Entered request is POST"
+        # create a form instance and populate it with data from the request:
+        form = AdminForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            print form.cleaned_data['cropName']
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = AdminForm()
+
+    return render(request, 'admin_form.html', {'form': form})
